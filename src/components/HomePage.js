ad2597879a8df3bb3072b9608acb543f057fa919
Leonardo Bitencourt Marques <leonardo.marques@dbserver.com.br>
2024-08-22 15:09:53 -0300

    update homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className="home-container">
       <div className="home-box">
         <h1>Hello World</h1>
-        <h2>GitHub CI + GitHub Pages</h2>
+        <h2>GitHub CI + GitHub Pages + Deploy automático</h2>
       </div>
     </div>
   );
